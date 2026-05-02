5f76b4c94c7d1b03b6b1a4f15aa2c37a35c118d5
yositomi <yositomi.art@gmail.com>
2026-05-02 20:10:26 +0900

    feat: Make overlay persistent across virtual desktops / 仮想デスクトップ間でのオーバーレイ表示の永続化
    - Add WS_EX_TOOLWINDOW flag to window extended styles
      (ウィンドウ拡張スタイルに WS_EX_TOOLWINDOW フラグを追加)
    - Ensure overlay visibility regardless of virtual desktop switching
      (仮想デスクトップの切り替えに関わらずオーバーレイの表示を維持)

--- OverlayWindow.xaml.cs
+++ OverlayWindow.xaml.cs
@@ -15,6 +15,7 @@ namespace ScreenDimmer
         const int GWL_EXSTYLE = -20;
         const int WS_EX_TRANSPARENT = 0x00000020;
         const int WS_EX_LAYERED = 0x00080000;
+        const int WS_EX_TOOLWINDOW = 0x00000080;
 
         public OverlayWindow()
         {
@@ -26,7 +27,7 @@ namespace ScreenDimmer
             base.OnSourceInitialized(e);
             var hwnd = new WindowInteropHelper(this).Handle;
             int extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
-            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT | WS_EX_LAYERED);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT | WS_EX_LAYERED | WS_EX_TOOLWINDOW);
         }
     }
 }
